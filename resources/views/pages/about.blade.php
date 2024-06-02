5e2988b42d33b3891af1e95b077ca9ce38b530dd
Kevin Rodriguez <kevinrodrihid2003@gmail.com>
2024-06-02 12:25:13 -0600

    Update CSS and about page layout
    The CSS file was modified to add a new style for the card class, setting the background color to white and removing the border. The about page layout was also updated to include two new cards with mission and vision information.
    
    ```

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,11 +1,10 @@
 @extends('layouts.app') 
-
 @section('content')
 <!-- About Area Start -->
-<section class="support-company-area fix pt-10 section-padding30">
-    <div class="support-wrapper align-items-end">
-        <div class="left-content">
-            <!-- section tittle -->
+
+<div class="container mt-5 mb-5">
+    <div class="row">
+        <div class="col-md-12">
             <div class="section-tittle mb-55">
                 <div class="front-text">
                     <h2 class="">Nosotros</h2>
@@ -25,12 +24,44 @@
                 <p>Contamos con profesionales y personal de amplia experiencia en el ramo. Nuestro valor agregado es la mejor atención personalizada, el mejor presupuesto y tiempo con un buen control de obra.</p>
             </div>
         </div>
-        <div class="right-content">
-            <div class="circle">
-                <img src="{{asset('/img/about/ecology.png')}}" alt="Descripción de la imagen">
+    </div>
+    <div class="row g-2">
+        <div class="col-md-6">
+            <div class="card p-3 text-center px-4">
+                <div class="user-image">
+                    <img src="{{asset('/img/about/ecology.png')}}" width="80" />
+                </div>
+
+                <div class="user-content">
+                    <h5 class="mb-3">Misión</h5>
+                    <p>
+                        Construir junto con nuestros clientes proyectos sólidos y perdurables que contribuyan a mejorar la calidad de vida de nuestros conciudadanos
+                    </p>
+                </div>
+                <div class="service-icon">
+                    <img src="{{ asset('img/icon/services_icon1.png') }}" alt="" />
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="card p-3 text-center px-4">
+                <div class="user-image">
+                    <img src="{{asset('/img/about/save-the-world.png')}}" width="80" />
+                </div>
+
+                <div class="user-content">
+                    <h5 class="mb-3">Visión</h5>
+                    <p>
+                        Ser una primera opción de nuestros clientes para realizar sus proyectos con calidad, seguridad y puntualidad, a nivel Nacional
+                    </p>
+                </div>
+                <div class="service-icon">
+                    <img src="{{ asset('img/icon/services_icon1.png') }}" alt="" />
+                </div>
             </div>
         </div>
     </div>
-</section>
+</div>
 <!-- About Area End -->
 @endsection
